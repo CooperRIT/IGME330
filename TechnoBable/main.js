02b0c56a32488d8e89ef08a4a50eeac16df86647
CooperRIT <112188791+CooperRIT@users.noreply.github.com>
2025-01-20 20:07:05 -0500

    Finished Technobabble

---
+++ TechnoBable/main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const words1 = ["Acute", "Aft", "Anti-matter", "Bipolar", "Cargo", "Command", "Communication", "Computer", "Deuterium", "Dorsal", "Emergency", "Engineering", "Environmental", "Flight", "Fore", "Guidance", "Heat", "Impulse", "Increased", "Inertial", "Infinite", "Ionizing", "Isolinear", "Lateral", "Linear", "Matter", "Medical", "Navigational", "Optical", "Optimal", "Optional", "Personal", "Personnel", "Phased", "Reduced", "Science", "Ship's", "Shuttlecraft", "Structural", "Subspace", "Transporter", "Ventral"];
+
+const words2 = ["Propulsion", "Dissipation", "Sensor", "Improbability", "Buffer", "Graviton", "Replicator", "Matter", "Anti-matter", "Organic", "Power", "Silicon", "Holographic", "Transient", "Integrity", "Plasma", "Fusion", "Control", "Access", "Auto", "Destruct", "Isolinear", "Transwarp", "Energy", "Medical", "Environmental", "Coil", "Impulse", "Warp", "Phaser", "Operating", "Photon", "Deflector", "Integrity", "Control", "Bridge", "Dampening", "Display", "Beam", "Quantum", "Baseline", "Input"];
+
+const words3 = ["Chamber", "Interface", "Coil", "Polymer", "Biosphere", "Platform", "Thruster", "Deflector", "Replicator", "Tricorder", "Operation", "Array", "Matrix", "Grid", "Sensor", "Mode", "Panel", "Storage", "Conduit", "Pod", "Hatch", "Regulator", "Display", "Inverter", "Spectrum", "Generator", "Cloud", "Field", "Terminal", "Module", "Procedure", "System", "Diagnostic", "Device", "Beam", "Probe", "Bank", "Tie-In", "Facility", "Bay", "Indicator", "Cell"];
+
+function GetRandomWord(wordArry) {
+    let randomNumber = Math.floor(Math.random() * (wordArry.length - 1));
+    return wordArry[randomNumber];
+}
+
+function GenerateFullRandomWord() {
+    document.querySelector("#output").innerHTML = `${GetRandomWord(words1)} ${GetRandomWord(words2)} ${GetRandomWord(words3)}`;
+}
+
+function init() {
+    document.querySelector("#myButton").onclick = GenerateFullRandomWord;
+}
+
+window.onload = init;
